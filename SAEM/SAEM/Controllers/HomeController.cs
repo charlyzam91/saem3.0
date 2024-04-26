8e60995215c6bf76abe9f591ee03e9548f05a436
PISA\Jcsamano <jcsamano@corpcab.com.mx>
2024-04-26 12:15:48 -0600

    Sube todo

--- SAEM/SAEM/Controllers/HomeController.cs
+++ SAEM/SAEM/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using SAEM.Models;
 using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
 
 namespace SAEM.Controllers
 {
@@ -15,6 +16,7 @@ namespace SAEM.Controllers
 
         public IActionResult Index()
         {
+            getEquipos();
             return View();
         }
 
@@ -27,6 +29,34 @@ namespace SAEM.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public void getEquipos()
+        {
+            List<Equipo> e = new List<Equipo>();
+            e.Add(new Equipo { idEquipo = 1, Estatus = 1, Marca = "Ensamblada", Modelo = "Ensamblado" });
+            e.Add(new Equipo { idEquipo = 2, Estatus = 2, Marca = "Ensamblada", Modelo = "Ensamblado" });
+            e.Add(new Equipo { idEquipo = 3, Estatus = 3, Marca = "Ensamblada", Modelo = "Ensamblado" });
+            e.Add(new Equipo { idEquipo = 4, Estatus = 1, Marca = "Ensamblada", Modelo = "Ensamblado" });
+            e.Add(new Equipo { idEquipo = 5, Estatus = 1, Marca = "Ensamblada", Modelo = "Ensamblado" });
+            e.Add(new Equipo { idEquipo = 6, Estatus = 1, Marca = "Ensamblada", Modelo = "Ensamblado" });
+            e.Add(new Equipo { idEquipo = 7, Estatus = 1, Marca = "Ensamblada", Modelo = "Ensamblado" });
+            e.Add(new Equipo { idEquipo = 8, Estatus = 1, Marca = "Ensamblada", Modelo = "Ensamblado" });
+            e.Add(new Equipo { idEquipo = 9, Estatus = 1, Marca = "Ensamblada", Modelo = "Ensamblado" });
+            e.Add(new Equipo { idEquipo = 10, Estatus = 1, Marca = "Ensamblada", Modelo = "Ensamblado" });
+            e.Add(new Equipo { idEquipo = 11, Estatus = 1, Marca = "Ensamblada", Modelo = "Ensamblado" });
+            e.Add(new Equipo { idEquipo = 12, Estatus = 1, Marca = "Ensamblada", Modelo = "Ensamblado" });
+            e.Add(new Equipo { idEquipo = 13, Estatus = 1, Marca = "Ensamblada", Modelo = "Ensamblado" });
+            e.Add(new Equipo { idEquipo = 14, Estatus = 1, Marca = "Ensamblada", Modelo = "Ensamblado" });
+            e.Add(new Equipo { idEquipo = 15, Estatus = 1, Marca = "Ensamblada", Modelo = "Ensamblado" });
+            e.Add(new Equipo { idEquipo = 16, Estatus = 1, Marca = "Ensamblada", Modelo = "Ensamblado" });
+            e.Add(new Equipo { idEquipo = 17, Estatus = 1, Marca = "Ensamblada", Modelo = "Ensamblado" });
+            e.Add(new Equipo { idEquipo = 18, Estatus = 1, Marca = "Ensamblada", Modelo = "Ensamblado" });
+            e.Add(new Equipo { idEquipo = 19, Estatus = 1, Marca = "Ensamblada", Modelo = "Ensamblado" });
+            e.Add(new Equipo { idEquipo = 20, Estatus = 1, Marca = "Ensamblada", Modelo = "Ensamblado" });
+
+            @ViewBag.Equipos = e;
+
         }
     }
 }
